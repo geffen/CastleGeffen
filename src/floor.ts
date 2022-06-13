fc56ebbabf12134c62312a16609a7213ad2dd6e8
Geffen <geffen@gmail.com>
2022-06-13 11:38:19 -0700

    +wearable scanner
    Wearable scanner added!

--- src/floor.ts
+++ src/floor.ts
@@ -1,68 +1,107 @@
-import { Noise } from '@dcl/noise-utils'
-// --- Set up a system ---
 
-class PerlinNoiseSystem implements ISystem {
-    group: Entity[] = []
-    active: boolean = false
-    timer: number = 0
-    update(dt: number) {
-        this.timer += dt / 4
 
-        // iterate over the entities of the group
-        for (let entity of this.group) {
-            // get the Transform component of the entity
-            const transform = entity.getComponent(Transform)
+//Let's make some ground first. Array handmade from estate shape.
+const ground = [
+    "0,0",
+    "0,16",
+    "16,0",
+    "16,16",
+    "0,32",
+    "16,32",
+    "0,48",
+    "16,48",
+    "48,32",
+    "48,48",
+    "32,48",
+    "32,32"]
 
-            // set the height of the entity based on a simplex3 function, using its position on the grid and the time
-            transform.position.y =
-                Noise.simplex3(
-                    transform.position.x / 16,
-                    this.timer,
-                    transform.position.z / 16
-                ) + 1
-        }
-    }
-}
+ground.forEach(function (value) {
+    var locArr = value.split(',')
+    //log("x:", Number(locArr[0]))
+    //log("y:", Number(locArr[1]))
 
-/// --- Spawner function ---
-let noiseSystem = new PerlinNoiseSystem()
-
-function spawnCube(box: Shape, x: number, y: number, z: number) {
+    let floor = new Entity()
+    floor.addComponent(new GLTFShape('models/ground.glb'))
+    floor.addComponent(
+        new Transform({
+            position: new Vector3(Number(locArr[0]) + 8, .00, Number(locArr[1]) + 8),
+        })
+    )
+    engine.addEntity(floor)
+})
+/*
+let terrain = new Entity()
+    terrain.addComponent(new GLTFShape('models/terrain.glb'))
+    terrain.addComponent(
+        new Transform({
+            position: new Vector3(0,0,0), rotation: Quaternion.Euler(0, 0, 0) 
+        })
+    )
+    engine.addEntity(terrain)
+    */
+    /*
+function spawnGrass(shape: Shape, x: number, y: number, z: number) {
     // create the entity
-    const cube = new Entity()
-    cube.addComponent(new Transform({ position: new Vector3(x, y, z) }))
-    cube.addComponent(box)
-    engine.addEntity(cube)
+    const grass = new Entity()
 
-    let col = new Material()
-    col.albedoColor = new Color3(x / 16, y / 16, z / 4)
-    cube.addComponent(col)
-
-    cube.addComponent(
-        new OnClick(() => {
-            if (!noiseSystem.active) {
-                engine.addSystem(noiseSystem)
-                noiseSystem.active = true
-            } else {
-                engine.removeSystem(noiseSystem)
-                noiseSystem.active = false
-            }
+    // add a transform to the entity
+    grass.addComponent(
+        new Transform({
+            position: new Vector3(x, y, z),
+            rotation: Quaternion.Euler(0, Math.random() * 30, 0),
+            scale: new Vector3(1, 0.5 + Math.random() / 2, 1),
         })
     )
 
-    noiseSystem.group.push(cube)
+    // add a shape to the entity
+    grass.addComponent(shape)
 
-    return cube
+    //grass.addComponent(new WaveGrass())
+
+    let col = new Material()
+    col.albedoColor = new Color3(x / 16, y / 16, z / 4)
+    grass.addComponent(col)
+
+    engine.addEntity(grass)
+
+    return grass
 }
 
-/// --- Reusable box shape ---
-let box = new BoxShape()
 
-/// --- Spawn the cubes as a grid ---
-for (var x = 0.5; x < 16; x++) {
-    for (var y = 0.5; y < 16; y++) {
-        let height = Noise.simplex3(x / 16, 0, y / 16) + 1
-        spawnCube(box, x, height, y)
+/// --- Spawn grass blades ---
+
+let grassShape = new GLTFShape('models/justflowers.glb')
+//let grass2Shape = new GLTFShape('models/grass2.glb')
+//let grass3Shape = new GLTFShape('models/grass3.glb')
+
+//Main strip
+
+for (var x = 1.5; x < 30; x++) {
+    for (var y = 1.5; y < 64; y++) {
+        // select a glb mesh randomly from the 3 variations
+        let selector = Math.random()
+
+        if (selector > 0.96) {
+            spawnGrass(grassShape, x, -.3, y)
+        } else if (selector < 0.1) {
+           // spawnGrass(grass2Shape, x, .1, y)
+        } else {
+            //spawnGrass(grass3Shape, x, .1, y)
+        }
     }
 }
+for (var x = 28; x < 63; x++) {
+    for (var y = 34; y < 63; y++) {
+        // select a glb mesh randomly from the 3 variations
+        let selector = Math.random()
 
+          if (selector > 0.96) {
+            spawnGrass(grassShape, x, -.3, y)
+        } else if (selector < 0.1) {
+            //spawnGrass(grass2Shape, x, .1, y)
+        } else {
+         //   spawnGrass(grass3Shape, x, .1, y)
+        }
+    }
+}
+*/
--- src/game.ts
+++ src/game.ts
@@ -1,4 +1,60 @@
-/// --- Set up a system ---
+import * as utils from "@dcl/ecs-scene-utils"
+import { movePlayerTo } from '@decentraland/RestrictedActions'
+
+import { WearablesScanner } from './scanner'
+import { buildScene } from './builderContent'
+import Door from './door'
+export const sceneMessageBus = new MessageBus()
+buildScene()
+
+const door = new Door(
+  new GLTFShape('models/Door_Fantasy.glb'),
+  {
+    position: new Vector3(37, 28.5, 51.15),
+    rotation: new Quaternion(0, -1, 0, 1),
+    scale: new Vector3(2.2, 1, 1.1)
+  },
+  'Open',
+  'Close'
+)
+
+const scanner = new WearablesScanner( //(35.6, 28.5, 48)(14.5, 0, 17.5)
+  { position: new Vector3(34, 27.5, 46), rotation: Quaternion.Euler(0, -60, 0) }, //todo:link to shop if you continue underneath (0, -220, 0)
+  'urn:decentraland:matic:collections-v2:0xe85f8e6e554da74cf402d36e13a6e93405fa0b91:0',
+  sceneMessageBus,
+  () => {
+    sceneMessageBus.emit('openDoor', {})
+    door.addComponentOrReplace(
+      new utils.Delay(5000, () => {
+        sceneMessageBus.emit('closeDoor', {})
+      })
+    )
+  },
+  () => {
+    sceneMessageBus.emit('closeDoor', {})
+  }
+)
+sceneMessageBus.on('scanning', () => {
+  scanner.scan()
+})
+
+sceneMessageBus.on('scanapprove', () => {
+  scanner.approve()
+})
+sceneMessageBus.on('scanreject', () => {
+  scanner.reject()
+  //door.toggle(true)
+})
+sceneMessageBus.on('openDoor', () => {
+  if (!door.isOpen) {
+    door.toggle(true)
+  }
+})
+sceneMessageBus.on('closeDoor', () => {
+  if (door.isOpen) {
+    door.toggle(false)
+  }
+})
 
 
 class RotatorSystem {
@@ -17,113 +73,84 @@ class RotatorSystem {
   }
 }
 
-// Add a new instance of the system to the engine
-engine.addSystem(new RotatorSystem())
 
-/// --- Spawner function ---
+//let FullBillboard = new Billboard() // rotate on all three axis
+let YBillboard = new Billboard(true, true ,true) // rotate only in theY axis
 
-function spawnOrigCube(x: number, y: number, z: number) {
+const respawner = new Entity()
+//respawner.addComponent(new BoxShape())
+//const tower = new GLTFShape("models/centerCastle.gtlf")
+respawner.addComponent(new GLTFShape('models/honorguard_storyteller.glb'))
+// add it to stare at camera
+respawner.addComponent(new Billboard())
+
+respawner.addComponent(new Transform({ position: new Vector3(19.1, 0, 21), scale: new Vector3(5, 5, 5) }))
+respawner.addComponent(
+  new OnPointerDown(
+    (e) => {
+      movePlayerTo({ x: 15, y: 120, z: 48 }, { x: 64, y: 0, z: 64 })
+      //NEED: a console tool to capture current location and angle OR can the second set of cordinates point to the current location of a model (user??)?
+    },
+    { hoverText: "Do you wish to visit the Castle?" }
+  )
+)
+engine.addEntity(respawner)
+
+
+// Let's place our castle. '
+function spawnCastle(x: number, y: number, z: number) {
     // create the entity
-    const cube = new Entity()
-
+    const castle = new Entity()
     // add a transform to the entity
-    cube.addComponent(new Transform({ position: new Vector3(x, y, z)}))
-
+    castle.addComponent(new Transform({ position: new Vector3(x, y, z), scale: new Vector3(.8, .6, .8), rotation: Quaternion.Euler(0, 270, 0) }))
     // add a shape to the entity
-    cube.addComponent(new BoxShape())
-
+    //cube.addComponent(new BoxShape())
+    castle.addComponent(new GLTFShape('models/Final_for_geffenz.glb'))
     // add the entity to the engine
-    engine.addEntity(cube)
-
-    return cube
+    engine.addEntity(castle)
+    return castle
 }
+spawnCastle(7,10,62)
 
-function spawnZcash(x: number, y: number, z: number) {
+
+function spawnStore(x: number, y: number, z: number) {
     // create the entity
-    const zcash = new Entity()
-    const randomz = Math.random() * 4 + 1
+    const store = new Entity()
     // add a transform to the entity
 
-    zcash.addComponent(new Transform({ position: new Vector3(x, y, z), scale: new Vector3(randomz, randomz, randomz) }))
+    store.addComponent(new Transform({ position: new Vector3(x, y, z), scale: new Vector3(1.3, 1.3, 1.3), rotation: Quaternion.Euler(0, 140, 0) }))
 
     // add a shape to the entity
     //cube.addComponent(new BoxShape())
-    zcash.addComponent(new GLTFShape('models/zcash.glb'))
+    store.addComponent(new GLTFShape('models/ramenshop.glb'))
 
     // add the entity to the engine
-    engine.addEntity(zcash)
+    engine.addEntity(store)
 
-    return zcash
+    return store
 }
 
-/// --- Spawn a cube, renamed to not conflict with floor.ts ---
+spawnStore(16,.1,16)
 
-/*const OrigCube = spawnOrigCube(8, 1, 8)
 
-OrigCube.addComponent(
-  new OnClick(() => {
-      OrigCube.getComponent(Transform).scale.z *= 1.1
-      OrigCube.getComponent(Transform).scale.x *= 0.9
 
-    spawnZcash(Math.random() * 8 + 1, Math.random() * 8, Math.random() * 8 + 1)
+
+const nftFrame = new Entity()
+const shapeComponent = new NFTShape(
+  "ethereum://0x8a90cab2b38dba80c64b7734e58ee1db38b8992e/373", //doodle
+  //"ethereum://0xe75113d4a417c2d33c67fb127b419e5f47c5d62c/6786", //potm
+  //"ethereum://0x49cf6f5d44e70224e2e23fdcdd2c053f30ada28b/13856", //clonex
+  {
+    style: PictureFrameStyle.None,
+  }
+)
+nftFrame.addComponent(shapeComponent)
+nftFrame.addComponent(
+  new Transform({
+
+    position: new Vector3(15.83, 1.6, 11.94), rotation: Quaternion.Euler(0, -40, 0), scale : new Vector3(2.3,2.75,2.3)
   })
-)*/
+)
+engine.addEntity(nftFrame)
 
- //--- add mushroom model and placement
-
-export function addMushroom(x:number,y:number,z:number) {
-    let mushroom = new Entity()
-    mushroom.addComponent(new GLTFShape('models/mushroom.glb'))
-    mushroom.addComponent(
-        new Transform({
-            //rotation: Quaternion.Euler(0, -180, 0),
-            position: new Vector3(x,y,z),
-            scale: new Vector3(.5,.5,.5)
-        })
-    )
-    engine.addEntity(mushroom)
-}
-
-// put mushrooms in the center of the 3 plots
-/*
-addMushroom(16, 0, 16)
-addMushroom(16, 0, 48)
-addMushroom(48, 0, 48)
-
-//make a bunch zcash everywhere
-
-for (let i = 0; i < 180; i++) {
-    spawnZcash(Math.random() * 16 + 8, Math.random() * 16, Math.random() * 16 + 8)
-}
-for (let i = 0; i < 180; i++) {
-    spawnZcash(Math.random() * 16 + 8, Math.random() * 16, Math.random() * 16 + 40)
-}
-for (let i = 0; i < 180; i++) {
-    spawnZcash(Math.random() * 16 + 40, Math.random() * 16, Math.random() * 16 + 40)
-}
-*/
-
-
-function spawnTower(x: number, y: number, z: number, scalex: number, scaley: number, scalez: number, rotatex: number) {
-    // create the entity
-    const tower = new Entity()
-    // add a transform to the entity
-    tower.withCollisions = true
-    tower.addComponent(new Transform({ position: new Vector3(x, y, z), scale: new Vector3(scalex, scaley, scalez), rotation: Quaternion.Euler(0, rotatex, 0) }))
-
-    // add a shape to the entity
-    //cube.addComponent(new BoxShape())
-    tower.addComponent(new GLTFShape('models/centerCastle.gltf'))
-
-    // add the entity to the engine
-    engine.addEntity(tower)
-
-    return tower
-}
-
-
-//Spawn terraforming project to dwarf neighbors.
-
-spawnTower(16, 0, 48, 0.5, 0.4, 0.7, 0)
-spawnTower(48, 0, 48, 0.5, 0.4, 0.7, 90)
-spawnTower(16, 0, 16, 0.5, 0.4, 0.7, 180)
+buildScene()
